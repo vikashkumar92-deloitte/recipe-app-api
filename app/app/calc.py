5e960ea61b6625cbd66ab95bfa0ed21c4722b331
Vikash Kumar <vikashkumar92@deloitte.com>
2022-07-12 10:06:10 +0530

    Install PostgreSQL database adapter

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Funtion
+"""
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x+y
+
+def sub(x, y):
+    """Substract y from x and return result"""
+    return x-y
